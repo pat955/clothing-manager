5a7bfad4d7efe789932512df220a20f75fa19197
pat955 <patricijap8@gmail.com>
2024-03-26 11:07:02 +0100

    with sudo, the app works

--- main.go
+++ main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"time"
+
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+func updateTime(clock *widget.Label) {
+	formatted := time.Now().Format("Time: 03:04:05")
+	clock.SetText(formatted)
+}
+
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Clock")
 
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
+	clock := widget.NewLabel("")
+	updateTime(clock)
 
+	w.SetContent(clock)
+	go func() {
+		for range time.Tick(time.Second) {
+			updateTime(clock)
+		}
+	}()
 	w.ShowAndRun()
 }
